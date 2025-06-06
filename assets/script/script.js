5a8d1bceb31b042faa5f3a33cea9c97a73e5f5b8
01Chloe <allier.chloe@gmaim.com>
2025-06-06 15:55:23 +0200

    add daily quote

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,6 +3,7 @@ const quoteText = document.querySelector("#quote-text");
 const quoteAuthor = document.querySelector("#quote-author");
 const quoteType = document.querySelector("#quote-type");
 const quoteList = document.querySelector("#quote-list");
+const dailyQuote = document.querySelector("#daily-quote");
 
 class Quote {
   constructor(text, author, type) {
@@ -18,6 +19,10 @@ let quotesArray = JSON.parse(quotesInStorage);
 if (quotesArray === null) {
   quotesArray = [];
 }
+
+let quotesListTitle = document.createElement("h2");
+quotesListTitle.innerHTML = "Ma liste de citations : ";
+quoteList.insertAdjacentElement("beforebegin", quotesListTitle);
 
 let errorMsg = document.createElement("p");
 form.insertAdjacentElement("afterend", errorMsg);
@@ -71,7 +76,7 @@ function displayQuote() {
       cardBody.classList.add("card-body");
       card.insertAdjacentElement("beforeend", cardBody);
 
-      let title = document.createElement("h2");
+      let title = document.createElement("h3");
       title.classList.add("card-title");
       title.innerHTML = quotesArray[i].text;
       cardBody.insertAdjacentElement("beforeend", title);
@@ -98,5 +103,44 @@ function displayQuote() {
       });
       cardBody.insertAdjacentElement("beforeend", deleteBtn);
     }
+  }
+}
+
+displayDailyQuote();
+function displayDailyQuote() {
+  dailyQuote.innerHTML = "";
+
+  let randomQuote = Math.floor(Math.random() * quotesArray.length);
+
+  if (quotesArray.length >= 1) {
+    let quotesDailyTittle = document.createElement("h2");
+    quotesDailyTittle.innerHTML = "Ma citation du jour";
+    dailyQuote.insertAdjacentElement("beforebegin", quotesDailyTittle);
+
+    let card = document.createElement("li");
+    card.classList.add("card");
+    card.style.width = "18rem";
+    dailyQuote.insertAdjacentElement("beforeend", card);
+
+    let cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    card.insertAdjacentElement("beforeend", cardBody);
+
+    let title = document.createElement("h3");
+    title.classList.add("card-title");
+    title.innerHTML = quotesArray[randomQuote].text;
+    cardBody.insertAdjacentElement("beforeend", title);
+
+    let author = document.createElement("p");
+    author.classList.add("card-text");
+    author.classList.add("fst-italic");
+    author.innerHTML = quotesArray[randomQuote].author;
+    cardBody.insertAdjacentElement("beforeend", author);
+
+    let type = document.createElement("p");
+    type.innerHTML = quotesArray[randomQuote].type;
+    cardBody.insertAdjacentElement("beforeend", type);
+  } else {
+    dailyQuote.innerHTML = "";
   }
 }
